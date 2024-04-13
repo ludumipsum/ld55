7f10569a06d66d3635f3ed1267d97ad54c6fb4f4
Alex Berghage <alex@bearcage.is>
2024-04-12 19:39:13 -0700

    Checkpoint from pairing

---
+++ PlayerCharacter.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@export var speed = 300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func get_input():
+	var input_vector := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	return input_vector
+
+
+func _physics_process(delta):
+	var input = get_input()
+	
+	set_velocity(input * speed)
+	move_and_slide()
