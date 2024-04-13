d5cf5426f111a1eec547dbacef2cad1a02bc65a8
Josh Verner <jver422@users.noreply.github.com>
2024-04-13 19:25:58 -0400

    fixed a broken path

--- Scenes/main menu/message_playgame.gd
+++ Scenes/main menu/message_playgame.gd
@@ -39,7 +39,7 @@ func action():
 	if yes == false:
 		hide_textbox()
 	if yes == true:
-		get_tree().change_scene_to_file("res://the_office.tscn")
+		get_tree().change_scene_to_file("res://Scenes/the_office.tscn")
 		pass
 
 
