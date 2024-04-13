1a1ab496c75bb86906cf45a2b5c77789477a0569
Josh Verner <jver422@users.noreply.github.com>
2024-04-13 01:02:56 -0400

    initial branch commit
    started work on a main menu scene

--- PlayerCharacter.gd
+++ PlayerCharacter.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 300.0
+@export var speed = 100.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
