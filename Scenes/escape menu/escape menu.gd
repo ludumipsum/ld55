19a79b97b9e0462e872dd40ad310ceb0ba4a3f1e
Josh Verner <jver422@users.noreply.github.com>
2024-04-14 14:54:50 -0400

    removed uneeded self

--- Scenes/escape menu/escape menu.gd
+++ Scenes/escape menu/escape menu.gd
@@ -4,13 +4,13 @@ var is_paused = false
 
 func _unhandled_input(event):
 	if event.is_action_pressed("pause"):
-		self.is_paused = !is_paused
+		is_paused = !is_paused
 		get_tree().paused = is_paused
 		visible = is_paused
 
 
 func _on_play_pressed():
-	self.is_paused = false
+	is_paused = false
 	get_tree().paused = is_paused
 	visible = is_paused
 
@@ -19,7 +19,7 @@ func _on_quit_pressed():
 
 
 func _on_main_menu_pressed():
-	self.is_paused = false
+	is_paused = false
 	get_tree().paused = is_paused
 	visible = is_paused
 	get_tree().change_scene_to_file("res://Scenes/main menu/main_menu.tscn")
