6cec3f7fb76ad057acfffa2b788a65102d04f340
Alex Berghage <alex@bearcage.is>
2024-04-13 21:23:54 -0700

    A working elevator! (#6)
    * checkpoint
    
    * A working elevator!
    
    ^title

---
+++ Scenes/Floors.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+signal change_floors(to: int)
+
+@export var active_floor: int = 3
+var floors: Array[Node2D] = []
+
+func initialize_floors():
+	for node in self.get_children():
+		floors.push_back(node)
+		self.remove_child(node)
+
+func change_floor_to(to: int):
+	# Put the current floor on the shelf
+	var children = self.get_children()
+	match children.size():
+		0:
+			pass
+		1:
+			var floor_to_reinsert = children.pop_front()
+			self.remove_child(floor_to_reinsert)
+			floors.insert(active_floor, floor_to_reinsert)
+		var N:
+			printerr("got too many floor children: ", N)
+		
+	# Take out the new floor and put it in the scene
+	active_floor = to
+	self.add_child(floors.pop_at(active_floor))
+	change_floors.emit(active_floor)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	initialize_floors()
+	change_floor_to(active_floor)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- minimap/minimap.gd
+++ minimap/minimap.gd
@@ -1,29 +1,25 @@
 extends Node2D
 
-@export var floors: Array[Node2D]
+@export var floor_controller: Node2D
 
 @onready var sprite = $ViewportSprite
 @onready var stack = $"Viewport/Floor Stack"
 @onready var viewport = $Viewport
 
-var active_floor: int = 0
+var active_floor: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var floors: Array[Node] = floor_controller.get_children()
+	active_floor = floors.size() - 1
 	sprite.texture = viewport.get_texture()
 	stack.floors = floors
 	stack.instantiate_floors()
-	self.change_floor_to(1)
+	self.change_floor_to(active_floor)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func update_floor_hiddens():
-	var idx = 0
-	for f in floors:
-		f.visible = true if active_floor == idx else false
-		idx += 1
 
 func change_floor_to(to: int):
 	active_floor = to
