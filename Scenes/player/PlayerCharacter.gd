b5d163a3be4ca198a20309558e7214d4e6aa6fa9
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2024-04-15 00:52:21 -0400

    Make texture atlases and animation more good

--- Scenes/player/PlayerCharacter.gd
+++ Scenes/player/PlayerCharacter.gd
@@ -3,83 +3,40 @@ class_name PlayerCharacter
 
 @export var speed = 100.0
 
+@onready var animation_tree = $AnimationTree
+
 var isfrozen = false
 var player = self
 
-
-@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
-var facing : Direction = Direction.UP
-var is_walking : bool = false
-
-enum Direction {RIGHT, DOWN, LEFT, UP}
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-func get_input():
+func get_new_velocity():
 	var input_vector := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	return input_vector
+	return input_vector * speed
 
 
 func _physics_process(_delta):
-	var input_vector = get_input()
-
-	var facing = get_facing(input_vector)
-	var is_walking = true if input_vector.length() > 0.1 else false
-	
-	if player.facing != facing || player.is_walking != is_walking:
-		player.facing = facing
-		player.is_walking = is_walking
-		update_animation()
-
-	player.velocity = input_vector * speed
+	velocity = get_new_velocity()
+	update_animation_state()
 	move_and_slide()
 
-
+func update_animation_state():
+	if velocity == Vector2.ZERO:
+		animation_tree["parameters/conditions/is_idle"] = true
+		animation_tree["parameters/conditions/is_moving"] = false
+	if velocity != Vector2.ZERO:
+		animation_tree["parameters/conditions/is_idle"] = false
+		animation_tree["parameters/conditions/is_moving"] = true
+		animation_tree["parameters/idle/blend_position"] = velocity
+		animation_tree["parameters/walk/blend_position"] = velocity
 
 func freeze():
 	if isfrozen == true:
 		player.set_physics_process(true)
 		player.set_process_input(true)
-		player.animated_sprite.play()
 		isfrozen = false
 	else:
 		player.set_physics_process(false)
 		player.set_process_input(false)
-		player.animated_sprite.pause()
 		isfrozen = true
-
-
-
-func get_facing(input_vector: Vector2) -> float:
-	if input_vector.length() < 0.1:
-		return player.facing
-
-	var a = input_vector.angle()
-	if a >= -(3.0/4.0)*PI && a <= -(1.0/4.0)*PI:
-		return Direction.UP
-	elif a > -(1.0/4.0)*PI && a < (1.0/4.0)*PI:
-		return Direction.RIGHT
-	elif a >= (1.0/4.0)*PI && a <= (3.0/4.0)*PI:
-		return Direction.DOWN
-	# spans [(3/4)PI..PI] and then [-PI..-(3/4)PI].
-	return Direction.LEFT
-
-func update_animation():
-	match [player.facing, player.is_walking]:
-		[Direction.LEFT, true]:
-			animated_sprite.play("walk_left")
-		[Direction.RIGHT, true]:
-			animated_sprite.play("walk_right")
-		[Direction.UP, true]:
-			animated_sprite.play("walk_up")
-		[Direction.DOWN, true]:
-			animated_sprite.play("walk_down")
-		[Direction.LEFT, false]:
-			animated_sprite.play("idle_left")
-		[Direction.RIGHT, false]:
-			animated_sprite.play("idle_right")
-		[Direction.UP, false]:
-			animated_sprite.play("idle_up")
-		[Direction.DOWN, false]:
-			animated_sprite.play("idle_down")
+		animation_tree["parameters/conditions/is_idle"] = true
+		animation_tree["parameters/conditions/is_moving"] = false
+#
